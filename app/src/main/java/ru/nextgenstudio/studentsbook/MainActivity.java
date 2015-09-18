ceb44d41bc18b8fec9761d957b5d864726ad90b6
Egor Machnev <egork1996@yandex.ru>
2015-09-19 02:07:32 +0300

    Created base project structure. Added activities and setted up colors

--- app/src/main/java/ru/nextgenstudio/studentsbook/MainActivity.java
+++ app/src/main/java/ru/nextgenstudio/studentsbook/MainActivity.java
@@ -1,30 +1,22 @@
 package ru.nextgenstudio.studentsbook;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SplashActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
-        }, 1500);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
--- app/src/main/java/ru/nextgenstudio/studentsbook/SettingsActivity.java
+++ app/src/main/java/ru/nextgenstudio/studentsbook/SettingsActivity.java
@@ -1,30 +1,22 @@
 package ru.nextgenstudio.studentsbook;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.preference.PreferenceActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SplashActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
-        }, 1500);
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
