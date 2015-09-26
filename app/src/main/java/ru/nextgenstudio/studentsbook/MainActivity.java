b7d679068d142fd896f1dfd1989e3f3aec5f7b6b
Egor Machnev <egork1996@yandex.ru>
2015-09-27 00:35:47 +0300

    Added toolbar. CHANGE ICONS COLOR IN TOOLBAR

--- app/src/main/java/ru/nextgenstudio/studentsbook/MainActivity.java
+++ app/src/main/java/ru/nextgenstudio/studentsbook/MainActivity.java
@@ -2,6 +2,7 @@ package ru.nextgenstudio.studentsbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null){
+
+            setSupportActionBar(toolbar);
+        }
     }
 
     @Override
